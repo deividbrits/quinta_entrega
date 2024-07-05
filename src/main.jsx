509e60c71dc131f60723b5776909340de60c3007
deividbrits <dabrito.dwfs13@bootcampudd.cl>
2024-07-04 23:12:21 -0400

    agrego paginas , no funciona layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Layout } from './components/Layout.jsx'
+import { About } from './pages/About.jsx'
+import { Preview } from './pages/Preview.jsx'
 
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-  },
+    elemet: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path:'/preview',
+        element: <Preview />
+      }
+    ],
+
+  }
+  
 ])
 
 
